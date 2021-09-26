b1fa0f4bad0a5aa527d3c56f86db168936cc4ec5
张诏 <2321885538@qq.com>
2021-09-26 10:29:07 +0800

    automatically generate files

---
+++ src/main/java/com/tyzz/blog/config/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.tyzz.blog.config;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Description:
+ *
+ * @Author: ZhangZhao
+ * DateTime: 2021-09-26 9:49
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String test(Exception e) {
+        return "error";
+    }
+}
---
+++ src/main/java/com/tyzz/blog/entity/exception/BlogException.java
@@ -0,0 +1,10 @@
+package com.tyzz.blog.entity.exception;
+
+/**
+ * Description:
+ *
+ * @Author: ZhangZhao
+ * DateTime: 2021-09-26 9:51
+ */
+public class BlogException extends RuntimeException {
+}
