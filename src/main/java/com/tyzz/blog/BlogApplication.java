3ebdccd01eca50bdb4ebea96f90a96aabdffa02f
张诏 <2321885538@qq.com>
2021-09-26 10:43:56 +0800

    automatically generate dao files

--- src/main/java/com/tyzz/blog/BlogApplication.java
+++ src/main/java/com/tyzz/blog/BlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.tyzz.blog.entity")
+@MapperScan("com.tyzz.blog.dao")
 public class BlogApplication {
 
     public static void main(String[] args) {
